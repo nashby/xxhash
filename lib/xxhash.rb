c90b5bd72fc25d37cf4de1aa27f7c56a192135b9
Malte Rohde <malte.rohde@flavoursys.com>
2013-09-02 18:31:43 +0200

    add streaming hash api

--- lib/xxhash.rb
+++ lib/xxhash.rb
@@ -5,4 +5,14 @@ module XXhash
   def self.xxh32(input, seed)
     Internal.xxh32(input, seed)
   end
+
+  def self.xxh32_stream(io, seed, chunk_size = 32)
+    raise 'wrong parameter type: should be Core::IO' unless io.kind_of?(IO) || io.kind_of?(StringIO)
+
+    sh = Internal::StreamingHash.new(seed)
+    until (chunk = io.read(chunk_size)).nil?
+      sh.update(chunk)
+    end
+    sh.digest
+  end
 end
--- test/xxhash_test.rb
+++ test/xxhash_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
+require 'stringio'
 
 describe XXhash do
   it 'returns hash' do
     assert_equal 2758658570, XXhash.xxh32('test', 123)
+  end
+
+  describe 'StreamingHash' do
+    it 'returns the hash for streamed strings' do
+      assert_equal 2758658570, XXhash.xxh32_stream(StringIO.new('test'), 123)
+    end
+
+    it 'returns the hash for streamed files' do
+      h1 = XXhash.xxh32(File.read(__FILE__), 123)
+      h2 = XXhash.xxh32_stream(File.open(__FILE__), 123)
+    end
   end
 end
