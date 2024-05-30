ef628ce9f82dcd50e11240156e32856bdb1c0eee
Jean byroot Boussier <jean.boussier+github@shopify.com>
2024-05-30 19:15:01 +0200

    Modernize Ruby C API ussage (#23)
    Convert the old `Data_` API calls into `TypedData`. This allows for
    write barriers, immediate free etc.
    
    Also undefined the allocators of the Data classes to avoid a warning
    on recent rubies.
    
    Co-authored-by: Jean Boussier <jean.boussier@gmail.com>

--- ext/xxhash/xxhash.h
+++ ext/xxhash/xxhash.h
@@ -19,14 +19,12 @@ typedef VALUE (ruby_method)();
 
 VALUE xxhash_xxh32(VALUE mod, VALUE input, VALUE seed);
 VALUE xxhash_xxh32_file(VALUE mod, VALUE filename, VALUE seed);
-void xxhash32_streaming_hash_free(xxhash_xxh32_t* state);
 VALUE xxhash32_streaming_hash_new(VALUE klass, VALUE seed);
 VALUE xxhash32_streaming_hash_update(VALUE self, VALUE data);
 VALUE xxhash32_streaming_hash_reset(VALUE self);
 VALUE xxhash32_streaming_hash_digest(VALUE self);
 VALUE xxhash_xxh64(VALUE mod, VALUE input, VALUE seed);
 VALUE xxhash_xxh64_file(VALUE mod, VALUE filename, VALUE seed);
-void xxhash64_streaming_hash_free(xxhash_xxh64_t* state);
 VALUE xxhash64_streaming_hash_new(VALUE klass, VALUE seed);
 VALUE xxhash64_streaming_hash_update(VALUE self, VALUE data);
 VALUE xxhash64_streaming_hash_reset(VALUE self);
